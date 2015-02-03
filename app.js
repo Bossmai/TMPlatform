234e601c4b41aa0116046f935a234055d65b4c2c
milesli1981 <milesli1981@icloud.com>
2015-02-03 22:26:50 +0800

    update to express 4

--- app.js
+++ app.js
@@ -1,43 +1,60 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , task = require('./routes/task')
-  , http = require('http')
-  , path = require('path');
+var routes = require('./routes/index');
+var task = require('./routes/task');
 
 var app = express();
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', routes);
+app.use('/task', task);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
 }
 
-app.get('/', routes.index);
-//app.get('/users', user.list);
-//app.get('/task', task.list);  
-app.get('/task/get', task.get);  
-//app.delete('/task/:id', task.delete);  
-//app.post('/task', task.add);  
-//app.put('/task/:id', task.update);  
-
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
+
+
+module.exports = app;
--- routes/task.js
+++ routes/task.js
@@ -1,32 +1,35 @@
-
-/*
- * GET home page.
- */
+var express = require('express');
+var router = express.Router();
 var mongo = require('mongodb')
-  , monk = require('monk')
-  , db = monk('localhost:27017/test');
+    , monk = require('monk')
+    , db = monk('localhost:27017/test');
+
+var fetchCount = 15;
+
+/* GET users listing. */
+router.get('/get', function(req, res, next) {
+    db.get('task').find({
+            'slaver.slaverMAC': req.query.slaverMAC,
+            'planExecDate':req.query.planExecDate},
+        function(err, docs) {
+            res.setHeader('Content-Type', 'application/json;charset=utf-8');
+            res.send(docs);
+
+        });
+
+    db.get("task").find()
+});
+
+router.get('/status', function(req, res, next) {
+    db.get('task').update({
+            'slaver.slaverMAC': req.query.slaverMAC,
+            'planExecDate':req.query.planExecDate},
+        function(err, docs) {
+            res.setHeader('Content-Type', 'application/json;charset=utf-8');
+            res.send(docs);
+
+        });
+});
 
 
-exports.get = function(req, res){
-	var me = this;
-	console.log(req.query);
-	db.get('task').find({
-			'slaver.slaverMAC': req.query.slaverMAC, 
-			'planExecDate':req.query.planExecDate}, 
-			function(err, docs) {
-		res.setHeader('Content-Type', 'application/json;charset=utf-8');  
-		res.send(docs);  
-		
-	});
-};
-
-exports.list = function(req, res){
-	var me = this;
-	console.log("list");
-	res.send("345"); 
-	
-	db.get('task').find({}, function(err, docs) {
-		res.setHeader('Content-Type', 'application/json;charset=utf-8');  
-		res.send(docs);  
-	});
-};
+module.exports = router;
