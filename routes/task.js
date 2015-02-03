ed8a7ae7fb2c4146bb8c913c86d91c0a09c0f238
milesli1981 <milesli1981@icloud.com>
2015-02-04 00:37:24 +0800

    IMPLEMENT SERVICE task/:status

--- routes/task.js
+++ routes/task.js
@@ -4,26 +4,19 @@ var mongo = require('mongodb')
     , monk = require('monk')
     , db = monk('localhost:27017/test');
 
-var fetchCount = 15;
+var fetchCount = {
+    limit: fetchCount
+};
 
 /* GET users listing. */
 router.get('/get', function(req, res, next) {
 
-    var searchCondition = {
-        'slaver.slaverMAC': req.query.slaverMAC,
-        'planExecDate':req.query.planExecDate,
-        'status':'NONE'
-    };
-    var limit = {
-        limit: 15
-    };
-
     db.get('task').find(
-        searchCondition,
-        limit,
+        getSearchCondition(req),
+        fetchCount,
         function(err, docs) {
 
-            db.get("task").update(searchCondition, limit, {
+            db.get("task").update(getSearchCondition(req), fetchCount, {
                 $set : {status: "INPROGRESS"}
             }, function(err, docs){
                 //TODO null;
@@ -34,16 +27,30 @@ router.get('/get', function(req, res, next) {
         });
 });
 
-router.get('/status', function(req, res, next) {
-    db.get('task').update({
-            'slaver.slaverMAC': req.query.slaverMAC,
-            'planExecDate':req.query.planExecDate},
+router.get('/:status', function(req, res, next) {
+    console.log("status= " + req.params.status);
+    db.get('task').update(getSearchCondition(req, 'INPROGRESS'),
+
+        {$set : {
+            status: req.params.status
+        }},
         function(err, docs) {
+            if(err){
+                res.setHeader('Content-Type', 'application/json;charset=utf-8');
+                res.send("failed with err: " + err);
+            }
             res.setHeader('Content-Type', 'application/json;charset=utf-8');
-            res.send(docs);
+            res.send("success");
 
         });
 });
 
+function getSearchCondition(req, status){
+    return {
+       'slaver.slaverMAC': req.query.slaverMAC,
+        'planExecDate':req.query.planExecDate,
+        'status': status? status:'NONE'
+    };
+}
 
 module.exports = router;
