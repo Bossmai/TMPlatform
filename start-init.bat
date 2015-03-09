34d7584f788e186124c1b7a4eed8d26a815075ab
milesli1981 <milesli1981@icloud.com>
2015-03-09 14:25:25 +0800

    add start-up batch

---
+++ start-init.bat
@@ -0,0 +1,8 @@
+del/q db
+mkdir db
+start cmd /c "mongod --dbpath db" 
+timeout /t 5
+mongoimport -d test -c phoneType --file %cd%\data\phoneType.dat
+mongoimport -d test -c slaver --file %cd%\data\slaver.dat
+npm start
+
---
+++ start.bat
@@ -0,0 +1,3 @@
+start cmd /c "mongod --dbpath db"
+npm start
+
